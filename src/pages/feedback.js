c7714dc3efcb13f594b4043bf1d9c2698ddb281b
PecheritsaAleksey <fromzeya@gmail.com>
2021-10-04 13:38:39 +0700

    Add form

---
+++ src/pages/feedback.js
@@ -0,0 +1,34 @@
+import React from "react"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const FeedbackPage = () => {
+  return (
+    <Layout>
+      <Seo title="Feedback" />
+      <h1>Send me your feedback!</h1>
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </Layout>
+  )
+}
+
+export default FeedbackPage
